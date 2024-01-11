c80dbd50f029197e0ead09dd86321bbada201fa8
Navin-Jongyin <63011210@kmitl.ac.th>
2024-01-12 02:21:20 +0700

    Edit

---
+++ lib/config/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color darkMainColor = Color(0xFF222831);
+  static const Color redMainColor = Color(0xFFF05454);
+  static const Color greyColor = Color(0xFFA9A9A9);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
