baec697ee399ec27aa9c6983c57942711e2d94f6
ynp44 <ext.yanapat.emd@truedigital.com>
2024-02-02 20:50:27 +0700

    edit profile added

--- lib/settingscreen/editProfilePage.dart
+++ lib/settingscreen/editProfilePage.dart
@@ -43,6 +43,111 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ],
         ),
       ),
+      body: Column(
+        children: [
+          Align(
+            child: Container(
+              margin: EdgeInsets.all(25),
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Palette.darkMainColor,
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 15),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      color: Palette.darkMainColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 15),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Surname",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      color: Palette.darkMainColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 15),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Phone Number",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      color: Palette.darkMainColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 15),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Bio",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      color: Palette.darkMainColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 50 , horizontal: 10),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
