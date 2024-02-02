83816353a34bbf84f34d3633d96fc39f909dd818
Navin-Jongyin <63011210@kmitl.ac.th>
2024-02-02 16:33:25 +0700

    commit

--- lib/mainscreen/newAuctionPage.dart
+++ lib/mainscreen/newAuctionPage.dart
@@ -1,16 +1,21 @@
 import 'package:bidsure_project/config/palette.dart';
 import 'package:bidsure_project/mainscreen/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart'; // Import Cupertino package
 import 'package:google_fonts/google_fonts.dart';
 
 class CreateAuctionPage extends StatefulWidget {
-  const CreateAuctionPage({super.key});
+  const CreateAuctionPage({Key? key}) : super(key: key);
 
   @override
   State<CreateAuctionPage> createState() => _CreateAuctionPageState();
 }
 
 class _CreateAuctionPageState extends State<CreateAuctionPage> {
+  String bidIncrease = '';
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,21 +52,95 @@ class _CreateAuctionPageState extends State<CreateAuctionPage> {
         child: Column(
           children: [
             Container(
-              margin: EdgeInsets.fromLTRB(20, 30, 20, 10),
+              margin: const EdgeInsets.fromLTRB(20, 30, 20, 10),
               child: TextFormField(
+                maxLength: 50,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  label: Text(
-                    "Item Name",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Palette.redMainColor),
+                  labelText: "Item Name",
+                  labelStyle: GoogleFonts.montserrat(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Palette.redMainColor,
                   ),
                   floatingLabelBehavior: FloatingLabelBehavior.always,
                 ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                minLines: 5,
+                maxLines: 10,
+                maxLength: 250,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: "Item Description",
+                  labelStyle: GoogleFonts.montserrat(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Palette.redMainColor,
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Price",
+                        labelStyle: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Palette.redMainColor,
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        suffixIcon: const Icon(Icons.attach_money),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        if (value.startsWith('0')) {
+                          if (value.length > 1 && value.length != 0) {
+                            setState(() {
+                              bidIncrease = value.replaceAll(RegExp('^0+'), '');
+                            });
+                          }
+                        } else {
+                          bidIncrease = value;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Bid Increase",
+                        labelStyle: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Palette.redMainColor,
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        suffixIcon: const Icon(Icons.attach_money),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
