398469a56a93ac751dffb955cdebf6b6d634688f
Navin-Jongyin <63011210@kmitl.ac.th>
2024-02-07 05:35:42 +0700

    redesign

--- lib/mainscreen/profilePage.dart
+++ lib/mainscreen/profilePage.dart
@@ -1,12 +1,11 @@
 import 'package:bidsure_project/config/bottomNavBar.dart';
 import 'package:bidsure_project/config/palette.dart';
 import 'package:bidsure_project/mainscreen/homePage.dart';
+import 'package:bidsure_project/mainscreen/myAuctionPage.dart';
 import 'package:bidsure_project/mainscreen/newAuctionPage.dart';
 import 'package:bidsure_project/mainscreen/searchPage.dart';
 import 'package:bidsure_project/mainscreen/walletPage.dart';
-import 'package:bidsure_project/mainscreen/myAuctionPage.dart';
 import 'package:bidsure_project/settingscreen/settingsPage.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -22,9 +21,10 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        shadowColor: Palette.darkMainColor,
         backgroundColor: Palette.whiteColor,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               "User Profile",
@@ -37,68 +37,60 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        const SettingsPage(),
-                  ),
-                );
-              },
-              icon: Icon(
-                Icons.settings,
-                color: Palette.greyColor,
-              ))
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const SettingsPage(),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.settings,
+              color: Palette.darkMainColor,
+            ),
+          )
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            child: Align(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
               child: Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.all(25),
-                    alignment: Alignment.center,
-                    height: 100,
-                    width: 100,
+                    height: 120,
+                    width: 120,
                     decoration: BoxDecoration(
-                      color: Palette.darkMainColor,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  Text(
-                    "Name Surename",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
+                        borderRadius: BorderRadius.circular(60),
                         color: Palette.darkMainColor),
                   ),
                   const SizedBox(
                     height: 20,
                   ),
+                  Text(
+                    "Name Surname",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Palette.darkMainColor,
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20),
+            const Divider(),
+            Flexible(
               child: ListView(
                 children: [
-                  ListTile(
-                    leading: ImageIcon(
-                      AssetImage("icons/auction.png"),
-                      size: 35,
-                    ),
-                    title: Text("My Auction",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Palette.darkMainColor,
-                        )),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                    onTap: () {
+                  buildListTile(
+                    "My Auction",
+                    Icons.handshake,
+                    Palette.darkMainColor,
+                    () {
                       Navigator.of(context).pushReplacement(
                         PageRouteBuilder(
                           pageBuilder:
@@ -108,99 +100,25 @@ class _ProfilePageState extends State<ProfilePage> {
                       );
                     },
                   ),
-                  ListTile(
-                    leading: ImageIcon(
-                      AssetImage("icons/payment.png"),
-                      size: 35,
-                    ),
-                    title: Text("Payment",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Palette.darkMainColor,
-                        )),
-                    trailing: Icon(Icons.arrow_forward_ios),
-
-                    // onTap: (){
-                    //   Navigator.of(context).pushReplacement(
-                    //     PageRouteBuilder(
-                    //       pageBuilder: (context, animation, secondaryAnimation) =>
-                    //       const MyAuctionPage(),
-                    //     ),
-                    //   );
-                    //
-                    // },
+                  buildListTile(
+                    "Payment",
+                    Icons.credit_card,
+                    Palette.darkMainColor,
+                    () {},
                   ),
-                  ListTile(
-                    leading: ImageIcon(
-                      AssetImage("icons/history.png"),
-                      size: 35,
-                    ),
-                    title: Text("History",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Palette.darkMainColor,
-                        )),
-                    trailing: Icon(Icons.arrow_forward_ios),
-
-                    // onTap: (){
-                    //   Navigator.of(context).pushReplacement(
-                    //     PageRouteBuilder(
-                    //       pageBuilder: (context, animation, secondaryAnimation) =>
-                    //       const MyAuctionPage(),
-                    //     ),
-                    //   );
-                    //
-                    // },
+                  buildListTile(
+                    "History",
+                    Icons.history,
+                    Palette.darkMainColor,
+                    () {},
                   ),
-                  ListTile(
-                    leading: ImageIcon(
-                      AssetImage("icons/shipping.png"),
-                      size: 35,
-                    ),
-                    title: Text("Shipping Address",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Palette.darkMainColor,
-                        )),
-                    trailing: Icon(Icons.arrow_forward_ios),
-
-                    // onTap: (){
-                    //   Navigator.of(context).pushReplacement(
-                    //     PageRouteBuilder(
-                    //       pageBuilder: (context, animation, secondaryAnimation) =>
-                    //       const MyAuctionPage(),
-                    //     ),
-                    //   );
-                    //
-                    // },
-                  ),
+                  buildListTile("Shipping Address", Icons.location_city,
+                      Palette.darkMainColor, () {})
                 ],
               ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Container(
-            width: 150.0,
-            margin: EdgeInsets.only(bottom: 50),
-            child: FloatingActionButton(
-              onPressed: () {},
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              backgroundColor: Palette.redMainColor,
-              child: Text(
-                "LOGOUT",
-                style: GoogleFonts.montserrat(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white),
-              ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: 4,
@@ -238,6 +156,34 @@ class _ProfilePageState extends State<ProfilePage> {
             );
           }
         },
+      ),
+    );
+  }
+
+  ListTile buildListTile(String title, IconData icon, Color textColor,
+      VoidCallback onTapCallBack) {
+    return ListTile(
+      title: Row(
+        children: [
+          Icon(
+            icon,
+            color: textColor,
+          ),
+          const SizedBox(width: 20),
+          Text(
+            title,
+            style: GoogleFonts.montserrat(
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+              color: textColor,
+            ),
+          ),
+        ],
+      ),
+      onTap: onTapCallBack,
+      trailing: const Icon(
+        Icons.arrow_forward_ios, // Right arrow icon
+        color: Palette.redMainColor,
       ),
     );
   }
