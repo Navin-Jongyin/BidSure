6b93eb6f18e29920b4d5443217137f23b13a8ec8
Navin-Jongyin <63011210@kmitl.ac.th>
2024-02-09 07:37:35 +0700

    create wallet page

--- lib/mainscreen/walletPage.dart
+++ lib/mainscreen/walletPage.dart
@@ -34,6 +34,104 @@ class _WalletPageState extends State<WalletPage> {
           ],
         ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Palette.darkMainColor,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "BidSure Wallet",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Palette.redMainColor,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Balance",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Palette.whiteColor),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "100.00",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 22,
+                                color: Palette.whiteColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          const ImageIcon(
+                            AssetImage("icons/baht.png"),
+                            color: Palette.whiteColor,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                height: 60,
+                width: 200,
+                child: FloatingActionButton(
+                  backgroundColor: Palette.redMainColor,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    "Top Up",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Palette.whiteColor),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: const Divider(),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: Text(
+                "Transaction History",
+                style: GoogleFonts.montserrat(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Palette.darkMainColor,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: 3,
         onTap: (index) {
