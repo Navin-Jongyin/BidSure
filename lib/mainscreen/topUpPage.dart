ac0e4745def5c8e514c8bbf5783695c4fb415db9
Navin-Jongyin <63011210@kmitl.ac.th>
2024-02-10 01:10:29 +0700

    edit top up page

--- lib/mainscreen/topUpPage.dart
+++ lib/mainscreen/topUpPage.dart
@@ -75,10 +75,215 @@ class _TopUpPageState extends State<TopUpPage> {
                 ),
               ),
               Container(
-                child: Text("Amount"),
+                margin: EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "Amount",
+                  style: GoogleFonts.montserrat(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Palette.darkMainColor),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "1000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "3000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "5000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "1000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "3000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Palette.greyColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 100,
+                    child: FloatingActionButton(
+                      backgroundColor: Palette.whiteColor,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "5000",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                textAlign: TextAlign.right, // Align input text to the right
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: "Enter Amount",
+                    prefixIcon: Icon(Icons.currency_bitcoin)),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        "Top Up Method",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Palette.greyColor),
+                          borderRadius: BorderRadius.circular(10)),
+                      width: 120,
+                      height: 100,
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Palette.whiteColor,
+                        child: Icon(
+                          Icons.credit_card,
+                          size: 30,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        padding: EdgeInsets.all(20),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Palette.redMainColor,
+          child: Text("Top Up",
+              style: GoogleFonts.montserrat(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Palette.whiteColor)),
         ),
       ),
     );
