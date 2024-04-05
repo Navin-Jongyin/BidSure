6375b7f3a801a85711e1e50fa7be0b2b7603905b
Navin-Jongyin <63011210@kmitl.ac.th>
2024-04-05 23:56:57 +0700

    edit

--- lib/components/palette.dart
+++ lib/components/palette.dart
@@ -9,4 +9,5 @@ class Palette {
   static const Color darkBlueColor = Color(0xFF07265B);
   static const Color darkGreyColor = Color(0xFF2e2e2e);
   static const Color backgroundColor = Color(0xFFF6F7Fb);
+  static const Color greenColor = Color(0xFF83c900);
 }
--- lib/pages/auctionPages/liveStreamAuction.dart
+++ lib/pages/auctionPages/liveStreamAuction.dart
@@ -1,8 +1,7 @@
-import 'package:bidsure_2/components/my_AlertDialog.dart';
 import 'package:bidsure_2/components/palette.dart';
+import 'package:bidsure_2/pages/home_Page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LiveAuction extends StatefulWidget {
@@ -13,138 +12,267 @@ class LiveAuction extends StatefulWidget {
 }
 
 class _LiveAuctionState extends State<LiveAuction> {
+  final TextEditingController priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            child: Container(
-              height: double.infinity,
-              color: Palette.greyColor,
-            ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Palette.backgroundColor,
           ),
           Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              height: 150,
-              width: double.infinity,
-              color: Palette.blueColor,
-              child: GestureDetector(
-                onTap: () {
-                  showDialog(
+            top: MediaQuery.of(context).size.height * 0.70,
+            right: 25,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 30,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Palette.blueColor,
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "+ 500",
+                          style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              color: Palette.whiteColor,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    showDialog(
                       context: context,
                       builder: (BuildContext context) {
-                        return MyAlertDialog();
-                      });
-                },
-                child: Align(
-                  alignment: Alignment.bottomCenter,
+                        return Theme(
+                          data: ThemeData.dark(),
+                          child: CupertinoAlertDialog(
+                            title: const Text(
+                              "Enter Price",
+                              style: TextStyle(
+                                  fontSize: 18, fontFamily: '.SF Pro Text'),
+                            ),
+                            content: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Enter Price to Bid. Once press confirm the process cannot be undo",
+                                  style: TextStyle(
+                                      fontSize: 13, fontFamily: '.SF Pro Text'),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                CupertinoTextField(
+                                  style: TextStyle(color: Palette.whiteColor),
+                                  controller: priceController,
+                                  placeholder: "Enter Price",
+                                  keyboardType: TextInputType.number,
+                                ),
+                              ],
+                            ),
+                            actions: <Widget>[
+                              CupertinoDialogAction(
+                                child: const Text(
+                                  "Cancel",
+                                  style: TextStyle(
+                                      color: Palette.blueColor,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              CupertinoDialogAction(
+                                child: Text(
+                                  "Place Bid",
+                                  style: TextStyle(
+                                      color: Palette.blueColor,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                onPressed: () {
+                                  print("Place Bid");
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
                   child: Container(
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Palette.whiteColor,
+                    width: 70.0,
+                    height: 70.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Palette.blueColor, // Adjust color as needed
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 3),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            color: Palette.greyColor)
+                      ],
                     ),
-                    child: Center(
-                      child: Text(
-                        "End Live",
-                        style: GoogleFonts.montserrat(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Palette.blueColor),
+                    child: const Center(
+                      child: ImageIcon(
+                        AssetImage("icons/bidlogo.png"),
+                        size: 30,
+                        color: Palette.whiteColor,
                       ),
                     ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned.fill(
+            child: SafeArea(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(hintText: "Comments"),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        child: const Icon(Icons.send),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
           ),
           Positioned(
-            left: 0,
-            right: 0,
-            bottom: 110,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(10),
-              height: 80,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Palette.whiteColor,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                    offset: Offset(0, 5),
-                    blurRadius: 10,
-                    spreadRadius: 2,
-                    color: Palette.darkGreyColor,
+            top: MediaQuery.of(context).size.height * .05,
+            right: 25,
+            child: SafeArea(
+              child: Container(
+                height: 50,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Palette.darkBlueColor,
+                ),
+                child: Center(
+                  child: Text(
+                    "01:02:31",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        color: Palette.whiteColor,
+                        fontWeight: FontWeight.bold),
                   ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Current",
-                          style: TextStyle(fontSize: 12),
-                        ),
-                        Text(
-                          "1000",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                  ),
-                  const VerticalDivider(),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Current",
-                          style: TextStyle(fontSize: 12),
-                        ),
-                        Text(
-                          "1000",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                  ),
-                  const VerticalDivider(),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Current",
-                          style: TextStyle(fontSize: 12),
-                        ),
-                        Text(
-                          "1000",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-          )
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * .07,
+            left: 25,
+            child: SafeArea(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                height: 80,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Palette.greyColor.withOpacity(0.3),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 25,
+                          width: 25,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Palette.redColor,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text("User 1")
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 22,
+                          child: Image.asset(
+                            "icons/baht.png",
+                            color: Palette.greenColor,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "1,200",
+                          style: GoogleFonts.montserrat(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Palette.greenColor),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              top: MediaQuery.sizeOf(context).height * 0.01,
+              left: 25,
+              child: SafeArea(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    height: 35,
+                    width: 35,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Palette.redColor,
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.close,
+                        color: Palette.whiteColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ))
         ],
       ),
     );
